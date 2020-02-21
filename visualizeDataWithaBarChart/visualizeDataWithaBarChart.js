4b0d732dc3bd40e2aa32a78ffb3174d723fec341
Isai-gomez <isaigomezhernandez@gmail.com>
2020-02-20 22:48:39 -0600

    pinto los rect dentro del svg

--- visualizeDataWithaBarChart/visualizeDataWithaBarChart.js
+++ visualizeDataWithaBarChart/visualizeDataWithaBarChart.js
@@ -1,20 +1,22 @@
-const url_api = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+const url_api =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
 const h = 1000;
 const w = 1000;
+const dataJson = { dato: null };
 
-async function getApi(url){  
+(async function(url) {
   let response = await fetch(url);
   let data = await response.json();
-  return data;   
-};
-const dataJson = {dato:getApi(url_api)};
-
-console.log("despues de la funcion"+dataJson);
-const  svg = d3.select('body')
-                .append('svg')
-                .attr('width', w)
-                .attr('height', h);
-      svg.selectAll("rect")
-          .data(dataJson.dato.data)
-          .enter()
-          .append("rect")
+  console.log(data);
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+  svg
+    .selectAll("rect")
+    .data(data.data)
+    .enter()
+    .append("rect");
+})(url_api);
