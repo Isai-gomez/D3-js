104dcbcf03308b961bc2cfb06b327653f17b2fcb
Isai-gomez <isaigomezhernandez@gmail.com>
2020-02-21 23:02:42 -0600

    esta muy unido no se distinge las barras

--- visualizeDataWithaBarChart/visualizeDataWithaBarChart.js
+++ visualizeDataWithaBarChart/visualizeDataWithaBarChart.js
@@ -3,22 +3,23 @@ const url_api =
 
 const h = 1000;
 const w = 1000;
-
+const barpadding = 3;
 (async function(url) {
   let response = await fetch(url);
   let data = await response.json();
   console.log(data);
   const div = d3
     .select("body")
-    .append("div")
+    .append("svg")
     .attr("width", w)
     .attr("height", h);
   div
-    .selectAll("div")
+    .selectAll("rect")
     .data(data.data)
     .enter()
-    .append("div")
-    .style("width", "2.8px")
-    .style("height", d => `${d[1]}px`)
-    .attr("class", "bar");
+    .append("rect")
+    .attr("width", (d, i) => `${i + 4}px`)
+    .attr("height", d => `${d[1]}px`)
+    .attr("x", (d, i) => `${i + 8}`)
+    .attr("y", d => `${h - d[1]}`);
 })(url_api);
