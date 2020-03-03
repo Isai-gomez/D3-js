1fb5ffcc737883f6bcada7b979b2a2da7c17932b
Isai-gomez <isaigomezhernandez@gmail.com>
2020-03-02 19:16:32 -0600

    funciona con live server

---
+++ BarChart/Const.js
@@ -0,0 +1,42 @@
+export const dataset = [
+  {
+    fruit: "Mango",
+    value: 45
+  },
+  {
+    fruit: "Uva",
+    value: 99
+  },
+  {
+    fruit: "Piña",
+    value: 23
+  },
+  {
+    fruit: "Melon",
+    value: 45
+  },
+  {
+    fruit: "Manzana",
+    value: 45
+  },
+  {
+    fruit: "Fresa",
+    value: 48
+  },
+  {
+    fruit: "Sandia",
+    value: 31
+  },
+  {
+    fruit: "Naranja",
+    value: 34
+  },
+  {
+    fruit: "Pera",
+    value: 14
+  },
+  {
+    fruit: "Pepino",
+    value: 75
+  }
+];
